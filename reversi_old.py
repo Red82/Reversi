713770b9f4c36fb95cf2b53e7ade7f0934c3d368
Red82 <andreyredkin82@gmail.com>
2013-07-26 10:06:17 +0300

    Second edition reversi.py

--- reversi_old.py
+++ reversi_old.py
@@ -4,13 +4,12 @@ import os, sys
 import json
 import urllib2
 from copy import deepcopy
-from pprint import pprint
-import operator
+from pprint import pprint 
 
 #API
 class Reversi:
     def __init__(self, ):
-        self.d_board = Board()
+        pass
     
     #Create HTTP Request, get Response.
     def send_req(self, request):
@@ -52,7 +51,7 @@ class Reversi:
 
     #Move checer
     def move(self, key, game_id, x, y):
-        data = self.send_req('/move/' + key + '/' + game_id + '/' + str(x) + ',' + str(y))
+        data = self.send_req('/move/' + key + '/' + game_id + '/' + x + ',' + y)
         return data
 
     #Check register your e-mail
@@ -72,9 +71,15 @@ class Reversi:
     def play(self, key, res):
         #x = raw_input('Please, enter x = ')
         #y = raw_input('Please, enter y = ')
-        
-        coords = self.d_board.moves(res['board'], res['current_turn'], res['server_color'])
-        res = self.move(key, res['id'], coords[0], coords[1])
+        if res['server_color'] == 1:
+            player = 2
+        else:
+            player = 1
+        d_board = Board(res['board'])
+        d_board.moves(player)
+        x = '6'
+        y = '2'
+        res = self.move(key, res['id'], x, y)
         return res
 
     #Show board
@@ -83,52 +88,76 @@ class Reversi:
 
 #Reversi playing AI
 class Board:
-    def __init__(self):
-        pass
+    def __init__(self, initial_state):
+        self.internal_state = deepcopy(initial_state)
+
+    #Move checer
+    def move(self, x, y, player):
+        new_state = Board(self.internal_state)
+        new_state.internal_state[x][y] = player
+        return new_state
 
     #Matrix brute force
-    def moves(self, state, player, enemy):
-        weights = {}
-        for y in xrange(len(state)-1):
-            for x in xrange(len(state[0]) - 1):
-                weights[(x, y)] = self.calc_move(x, y, state, player, enemy)
-                pprint({(x, y) : weights[(x, y)]})
-        sorted_dict = sorted(weights.iteritems(), key=operator.itemgetter(1))
-        return sorted_dict[-1][0]         
+    def moves(self, player):
+        for i in xrange(len(self.internal_state) - 1):
+            for j in xrange(len(len(self.internal_state[0])) - 1):
+                if self.is_move_acceptable(i, j, player):
+                    yield self.move(i, j, player)
 
     #Сheck matrix cells
-    def calc_move(self, x, y, state, player, enemy):
-        if state[y][x] != 0:
-            return 0
+    def is_move_acceptable(self, x, y, player):
+        if self.internal_state[x][y] != 0:
+            return False
 
+        result = False
 
-        weight = 0
         for direction in ((-1, -1), (0, -1), (1, -1),
-                            (-1, 0), (1, 0),
-                            (-1, 1), (0, 1), (1, 1)):
+                          (-1, 0), (1, 0),
+                          (-1, 1), (0, 1), (1, 1)):
 
-            weight += self.slice_direction(direction, state, x, y, player, enemy)
-            
-        return weight
+            slice = self.slice_direction(direction, x, y)
+
+            if self.is_valid_pattern(slice, player):
+                return True                 
+
+        return result
 
        # TODO: should generate the list of all cells values from the current point to the board side (like a slice)
-    def slice_direction(self, direction, state, x, y, player, enemy):
-        line = []
-        x += direction[0]
-        y += direction[1]
-        while ((0 <= x < len(state[0])) and
-                (0 <= y < len(state)) and
-                (state[y][x] == enemy)):
-            line.append([x, y])
-            x += direction[0]
-            y += direction[1]
-            
-        if ((0 <= x < len(state[0])) and
-                (0 <= y < len(state)) and
-                (state[y][x] == player)):    
-            return len(line)
-        return 0
+    def slice_direction(self, direction, x, y):
 
+        cursor = [x, y]
+        line = []
+
+        while 0 <= cursor[0] < len(len(self.internal_state[0]-1)) and
+                0 <= cursor[1] < len(self.internal_state[1]-1):
+
+            cursor[0] += direction[0]
+            cursor[1] += direction[1]
+            line.append(self.internal_state[[cursor[0]], [cursor[1]]])
+        
+        
+        # cursor = [x, y]
+        #
+        # line = []
+        #
+        # while 0 <= cursor[0] < len(len(self.internal_state[0])) and 0 <= cursor[1] < len(self.internal_state[1]):
+        #     cursor[0] += direction[0]
+        #     cursor[1] += direction[1]
+        #     line.append(self.internal_state[[cursor[0], cursor[1]]])
+    
+        pass
+
+    # Validates the direction slice for potential revert presence.
+    # TODO: can be implemented using simple Finite State Machine (you can use just a regex) or just ad-hoc
+    def is_valid_pattern(self, slice, player):
+
+        print('is_valid_pattern not implemented yet')
+        return True    
+
+   #Show board
+    def print_self(self):
+        pprint(self.internal_state)
+        
     
 def main():
     confirm = 1
